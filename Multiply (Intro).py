ed19f2da90d3345cd00c2e995f7cd99d2a6d0d7f
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-09-26 13:51:10 -0500

    Create Multiply (Intro).py

---
+++ Multiply (Intro).py
@@ -0,0 +1,8 @@
+def mult_two(a, b):
+    return a * b
+if __name__ == '__main__':
+    print('Example')
+    print(mult_two(5, 10))
+    assert mult_two(3, 2) == 6
+    assert mult_two(0, 1) == 0
+print("The first mission is done! Click 'Check' to earn cool rewards!")
